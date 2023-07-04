3437215d4ce30562dc92d784eede06d080221584
Honey <yurjune@naver.com>
2023-07-04 23:18:25 +0900

    feat: Per-page layout settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
+import { AppPropsWithLayout } from '@/model';
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page);
+
+  return getLayout(<Component {...pageProps} />);
 }
