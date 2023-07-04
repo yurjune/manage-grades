a73a14fbd80da685e109a1e82f1ffa40625ffbac
yurjune <yurjune@naver.com>
2023-07-05 02:27:59 +0900

    style: 로그인 view 수정

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,40 +1,25 @@
 import { AuthLayout } from '@/components';
 import { NextPageWithLayout } from '@/model';
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Input,
-  Checkbox,
-  Button,
-} from '@material-tailwind/react';
 import { ReactNode } from 'react';
+import { Card, CardBody, CardFooter, Typography, Input, Button } from '@material-tailwind/react';
 
 const SigninPage: NextPageWithLayout = () => {
   return (
     <Card className='w-96'>
-      <CardBody className='flex flex-col gap-4'>
+      <CardBody className='flex flex-col gap-4 mb-4'>
         <Typography className='self-center mb-4' variant='h3' color='black'>
           로그인
         </Typography>
         <Input label='Email' size='lg' />
         <Input label='Password' size='lg' />
-        <div className='-ml-2.5'>
-          <Checkbox label='Remember Me' />
-        </div>
       </CardBody>
-      <CardFooter className='pt-0'>
-        <Button variant='gradient' fullWidth>
-          Sign In
+      <CardFooter className='pt-0 flex flex-col gap-4'>
+        <Button className='text-sm' variant='gradient' fullWidth>
+          로그인
         </Button>
-        <Typography variant='small' className='mt-6 flex justify-center'>
-          Don't have an account?
-          <Typography as='a' href='#signup' variant='small' color='blue' className='ml-1 font-bold'>
-            Sign up
-          </Typography>
-        </Typography>
+        <Button className='text-sm' variant='gradient' fullWidth color='pink'>
+          테스트 계정 이용하기
+        </Button>
       </CardFooter>
     </Card>
   );
