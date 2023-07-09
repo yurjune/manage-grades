083f6481d995b4055af72f917ec9d9f79feb2f09
yurjune <yurjune@naver.com>
2023-07-10 05:28:59 +0900

    feat: 헤더 수정: logout, link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
+import { AuthContext } from '@/context';
 import { Button, Navbar, Typography } from '@material-tailwind/react';
+import Link from 'next/link';
+import { useContext } from 'react';
 
 export const Header = () => {
+  const { logout } = useContext(AuthContext);
+
   return (
     <Navbar className='sticky top z-10 h-max max-w-full rounded-none py-3 px-6'>
       <div className='flex items-center justify-between text-blue-gray-900'>
-        <Typography as='a' href='#' className='mr-4 cursor-pointer py-1.5 font-medium'>
-          성적 관리 시스템
+        <Typography className='mr-4 cursor-pointer py-1.5 font-bold'>
+          <Link href='/'>성적 관리 시스템</Link>
         </Typography>
         <div className='flex items-center gap-4'>
-          <Button variant='gradient' size='sm' className='inline-block'>
-            <span>로그아웃</span>
+          <Button variant='gradient' size='sm' className='inline-block' onClick={logout}>
+            로그아웃
           </Button>
         </div>
       </div>
