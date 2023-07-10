586204b39ef21b65e6197e881ba05fc51ef0f991
Honey <yurjune@naver.com>
2023-07-11 00:49:41 +0900

    style: material tailwind customizing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,41 @@ import { AppPropsWithLayout } from '@/model';
 import { store } from '@/redux/store';
 import '@/styles/globals.css';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-tailwind/react';
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
 
   return getLayout(
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider value={customTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>,
   );
 }
+
+const customTheme = {
+  input: {
+    defaultProps: {
+      color: 'indigo',
+    },
+  },
+  select: {
+    defaultProps: {
+      color: 'indigo',
+    },
+  },
+  button: {
+    defaultProps: {
+      color: 'indigo',
+    },
+  },
+  iconButton: {
+    defaultProps: {
+      color: 'indigo',
+    },
+  },
+};
 
 export default App;
