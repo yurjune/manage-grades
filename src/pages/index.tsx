2fbc73537063db4930a36daefa708b6326b120c3
Honey <yurjune@naver.com>
2023-07-11 03:01:45 +0900

    feat: home, /scores 페이지에 서버사이드 렌더링 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { CustomTabs, DashboardLayout, SpinnerContainer, StudentDialog } from '@/components';
+import { CustomTabs, DashboardLayout, StudentDialog } from '@/components';
 import { Table, TableBody, TableHead } from '@/components/StudentsTable';
 import { AuthProvider } from '@/context';
 import type { NextPageWithLayout } from '@/model';
 import { openStudentDialog, toggleStudentDialog } from '@/redux/features/dialogs/dialogsSlice';
 import { selectStudent } from '@/redux/features/student/studentSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { useGetStudentsQuery } from '@/redux/services/firestoreApi';
+import { firestoreApi, useGetStudentsQuery } from '@/redux/services/firestoreApi';
+import { wrapper } from '@/redux/store';
 import { Button } from '@material-tailwind/react';
 import { Fragment, ReactNode, useState } from 'react';
 
@@ -16,10 +17,20 @@ const TAB_FIELDS = [
   { label: 'B반', value: 'B' },
   { label: 'C반', value: 'C' },
 ];
+const initialGroupValue = TAB_FIELDS[0].value;
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async () => {
+  store.dispatch(firestoreApi.endpoints.getStudents.initiate({ group: initialGroupValue }));
+  await Promise.all(store.dispatch(firestoreApi.util.getRunningQueriesThunk()));
+
+  return {
+    props: {},
+  };
+});
 
 const Home: NextPageWithLayout = () => {
-  const [tabValue, setTabValue] = useState(TAB_FIELDS[0].value);
-  const { data: students, isLoading } = useGetStudentsQuery({ group: tabValue });
+  const [tabValue, setTabValue] = useState(initialGroupValue);
+  const { data: students = [] } = useGetStudentsQuery({ group: tabValue });
   const open = useAppSelector((state) => state.dialogs.studentDialogOpen);
   const selectedStudent = useAppSelector((state) => state.student.value);
   const dispatch = useAppDispatch();
@@ -34,14 +45,10 @@ const Home: NextPageWithLayout = () => {
       <div className='mb-4'>
         <CustomTabs fields={TAB_FIELDS} value={tabValue} onChange={(val) => setTabValue(val)} />
       </div>
-      {isLoading ? (
-        <SpinnerContainer />
-      ) : (
-        <Table>
-          <TableHead fields={TABLE_FIELDS} />
-          <TableBody rows={students ?? []} />
-        </Table>
-      )}
+      <Table>
+        <TableHead fields={TABLE_FIELDS} />
+        <TableBody rows={students} />
+      </Table>
       <Button className='mt-4 float-right' onClick={() => dispatch(openStudentDialog())}>
         학생 추가
       </Button>
--- src/pages/scores/index.tsx
+++ src/pages/scores/index.tsx
@@ -1,11 +1,13 @@
-import { CustomTabs, DashboardLayout, ScoreDialog, SpinnerContainer } from '@/components';
+import { CustomTabs, DashboardLayout, ScoreDialog } from '@/components';
 import { Table, TableBody, TableHead } from '@/components/SemestersTable';
 import { AuthProvider } from '@/context';
 import { useSelect } from '@/hooks';
+import { NextPageWithLayout } from '@/model';
 import { openScoreDialog, toggleScoreDialog } from '@/redux/features/dialogs/dialogsSlice';
 import { selectStudent } from '@/redux/features/student/studentSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { useGetStudentsQuery } from '@/redux/services/firestoreApi';
+import { firestoreApi, useGetStudentsQuery } from '@/redux/services/firestoreApi';
+import { wrapper } from '@/redux/store';
 import { Button, Option, Select } from '@material-tailwind/react';
 import { Fragment, ReactNode, useState } from 'react';
 
@@ -16,12 +18,22 @@ const TAB_FIELDS = [
   { label: 'B반', value: 'B' },
   { label: 'C반', value: 'C' },
 ];
+const initialGroupValue = TAB_FIELDS[0].value;
 const SEMESTER_FIELDS = ['23-1', '23-2'];
 
-const ScoresPage = () => {
-  const [tabValue, setTabValue] = useState(TAB_FIELDS[0].value);
+export const getServerSideProps = wrapper.getServerSideProps((store) => async () => {
+  store.dispatch(firestoreApi.endpoints.getStudents.initiate({ group: initialGroupValue }));
+  await Promise.all(store.dispatch(firestoreApi.util.getRunningQueriesThunk()));
+
+  return {
+    props: {},
+  };
+});
+
+const ScoresPage: NextPageWithLayout = () => {
+  const [tabValue, setTabValue] = useState(initialGroupValue);
   const [semester, handleSemesterChange] = useSelect(SEMESTER_FIELDS[0]);
-  const { data: students = [], isLoading } = useGetStudentsQuery({ group: tabValue });
+  const { data: students = [] } = useGetStudentsQuery({ group: tabValue });
   const open = useAppSelector((state) => state.dialogs.scoreDialogOpen);
   const selectedStudent = useAppSelector((state) => state.student.value);
   const dispatch = useAppDispatch();
@@ -52,14 +64,10 @@ const ScoresPage = () => {
           <CustomTabs fields={TAB_FIELDS} value={tabValue} onChange={(val) => setTabValue(val)} />
         </div>
       </div>
-      {isLoading ? (
-        <SpinnerContainer />
-      ) : (
-        <Table>
-          <TableHead fields={TABLE_FIELDS} />
-          <TableBody rows={students ?? []} semester={semester} />
-        </Table>
-      )}
+      <Table>
+        <TableHead fields={TABLE_FIELDS} />
+        <TableBody rows={students ?? []} semester={semester} />
+      </Table>
       <Button onClick={() => dispatch(openScoreDialog())} className='mt-4 float-right'>
         성적 추가
       </Button>
