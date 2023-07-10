e011be2964e4c3f72fac13f6b91e77c2d6a69f45
Honey <yurjune@naver.com>
2023-07-10 15:31:33 +0900

    feat: CustomTabs 컴포넌트 추가

---
+++ src/components/CustomTabs.tsx
@@ -0,0 +1,21 @@
+import { Tab, Tabs, TabsHeader } from '@material-tailwind/react';
+
+interface CustomTabsProps {
+  fields: { label: string; value: string }[];
+  value?: string;
+  onChange?: (v: string) => void;
+}
+
+export const CustomTabs = ({ fields, value, onChange }: CustomTabsProps) => {
+  return (
+    <Tabs value={value} className='mb-4'>
+      <TabsHeader>
+        {fields.map(({ label, value }) => (
+          <Tab key={value} value={value} onClick={() => onChange?.(value)}>
+            {label}
+          </Tab>
+        ))}
+      </TabsHeader>
+    </Tabs>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export * from './StudentDialog';
 export * from './Sidebar';
 export * from './DeleteConfirmDialog';
 export * from './SpinnerContainer';
+export * from './CustomTabs';
