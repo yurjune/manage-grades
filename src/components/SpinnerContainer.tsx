02a647e6234d1a272476d9b784a02b67bc1b39a0
Honey <yurjune@naver.com>
2023-07-11 01:45:24 +0900

    style: spinner color indigo

--- src/components/SpinnerContainer.tsx
+++ src/components/SpinnerContainer.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const SpinnerContainer = () => {
   return (
     <div className='w-full h-full flex justify-center items-center'>
-      <Spinner className='h-10 w-10' />
+      <Spinner color='indigo' className='h-10 w-10' />
     </div>
   );
 };
