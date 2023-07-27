c5e6ab7d37d2d66413475db6512c464b499531c4
Honey <yurjune@naver.com>
2023-07-27 20:16:50 +0900

    feat: StudentDialog 에 validation 적용

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -5,14 +5,18 @@ import { FieldErrors } from 'react-hook-form';
 interface ErrorMessageProps {
   errors: FieldErrors;
   name: string;
+  mt?: string;
+  mb?: string;
 }
 
-export const ErrorMessage = ({ errors, name }: ErrorMessageProps) => {
+export const ErrorMessage = ({ errors, name, mt, mb }: ErrorMessageProps) => {
   return (
     <HFErrorMEssage
       errors={errors}
       name={name}
-      render={({ message }) => <span className='text-red-500 text-sm'>{message}</span>}
+      render={({ message }) => (
+        <span className={`text-red-500 text-sm mt=${mt} mb=${mb}`}>{message}</span>
+      )}
     />
   );
 };
--- src/components/StudentDialog.tsx
+++ src/components/StudentDialog.tsx
@@ -13,19 +13,24 @@ import {
   type DialogProps,
 } from '@material-tailwind/react';
 import { Controller, useForm } from 'react-hook-form';
+import { ErrorMessage } from './ErrorMessage';
+
+const NAME = 'name';
+const GENDER = 'gender';
+const GRADE = 'grade';
+const GROUP = 'group';
+type FormValue = Pick<Student, 'name' | 'gender' | 'grade' | 'group'>;
 
 interface StudentDialogProps extends Pick<DialogProps, 'open'> {
   handleDialog: () => void;
   selectedStudent: Student | null;
 }
 
-type FormValue = Pick<Student, 'name' | 'gender' | 'grade' | 'group'>;
-
 export const StudentDialog = ({ open, handleDialog, selectedStudent }: StudentDialogProps) => {
   const [addStudent] = useAddStudentMutation();
   const [editStudent] = useEditStudentMutation();
 
-  const { register, handleSubmit, control, reset } = useForm<FormValue>({
+  const { register, handleSubmit, control, reset, formState } = useForm<FormValue>({
     values: {
       name: selectedStudent?.name ?? '',
       gender: selectedStudent?.gender ?? '',
@@ -34,7 +39,7 @@ export const StudentDialog = ({ open, handleDialog, selectedStudent }: StudentDi
     },
   });
 
-  const handleSumbitClick = async (values: FormValue) => {
+  const onSubmit = handleSubmit(async (values) => {
     if (selectedStudent) {
       await editStudent({ ...values, uid: selectedStudent.uid });
     } else {
@@ -43,9 +48,9 @@ export const StudentDialog = ({ open, handleDialog, selectedStudent }: StudentDi
 
     reset();
     handleDialog();
-  };
+  });
 
-  const required = { required: true };
+  const required = { required: '값을 입력해주세요!' };
   return (
     <Dialog size='sm' open={open} handler={handleDialog} className='bg-transparent shadow-none'>
       <Card className='mx-auto w-full'>
@@ -53,58 +58,66 @@ export const StudentDialog = ({ open, handleDialog, selectedStudent }: StudentDi
           <Typography variant='h4' color='black' className='mb-8'>
             {selectedStudent ? '학생 수정' : '학생 추가'}
           </Typography>
-          <form className='flex flex-col gap-8'>
-            <Input {...register('name', required)} label='이름' size='lg' />
-            <Controller
-              name='gender'
-              control={control}
-              rules={required}
-              render={({ field }) => (
-                <Select {...field} label='성별'>
-                  {['남', '여'].map((val) => (
-                    <Option key={val} value={val}>
-                      {val}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            />
-            <Controller
-              name='grade'
-              control={control}
-              rules={required}
-              render={({ field }) => (
-                <Select {...field} label='학년'>
-                  {['1', '2', '3'].map((val) => (
-                    <Option key={val} value={val}>
-                      {val}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            />
-            <Controller
-              name='group'
-              control={control}
-              rules={required}
-              render={({ field }) => (
-                <Select {...field} label='반'>
-                  {['A', 'B', 'C'].map((val) => (
-                    <Option key={val} value={val}>
-                      {val}
-                    </Option>
-                  ))}
-                </Select>
-              )}
-            />
+          <form className='flex flex-col gap-6'>
+            <div className='flex flex-col gap-2'>
+              <Input {...register(NAME, required)} label='이름' size='lg' />
+              <ErrorMessage name={NAME} errors={formState.errors} />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Controller
+                name={GENDER}
+                control={control}
+                rules={required}
+                render={({ field }) => (
+                  <Select {...field} label='성별'>
+                    {['남', '여'].map((val) => (
+                      <Option key={val} value={val}>
+                        {val}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              />
+              <ErrorMessage name={GENDER} errors={formState.errors} />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Controller
+                name={GRADE}
+                control={control}
+                rules={required}
+                render={({ field }) => (
+                  <Select {...field} label='학년'>
+                    {['1', '2', '3'].map((val) => (
+                      <Option key={val} value={val}>
+                        {val}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              />
+              <ErrorMessage name={GRADE} errors={formState.errors} />
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Controller
+                name={GROUP}
+                control={control}
+                rules={required}
+                render={({ field }) => (
+                  <Select {...field} label='반'>
+                    {['A', 'B', 'C'].map((val) => (
+                      <Option key={val} value={val}>
+                        {val}
+                      </Option>
+                    ))}
+                  </Select>
+                )}
+              />
+              <ErrorMessage name={GROUP} errors={formState.errors} />
+            </div>
           </form>
         </CardBody>
         <CardFooter className='pt-0'>
-          <Button
-            variant='gradient'
-            onClick={handleSubmit(handleSumbitClick)}
-            className='float-right'
-          >
+          <Button variant='gradient' onClick={onSubmit} className='float-right'>
             등록
           </Button>
         </CardFooter>
