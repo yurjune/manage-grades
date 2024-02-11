005a4247fb0becb7ea7ebc74bff8b14924b347a7
yurjune <yurjune@naver.com>
2024-02-11 23:34:05 +0900

    refactor: DashboardLayout

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import { Header, Sidebar } from '@/components';
+import { useAuth } from '@/shared/hooks/useAuth';
 import { Typography } from '@material-tailwind/react';
 import { PropsWithChildren } from 'react';
 
@@ -7,16 +8,21 @@ interface DashboardLayoutProps {
 }
 
 export const DashboardLayout = ({ title, children }: PropsWithChildren<DashboardLayoutProps>) => {
+  const { logout } = useAuth();
+
   return (
     <div className='w-full h-full'>
       <main className='h-full my-0 mx-auto max-w-[90rem] flex'>
         <Sidebar />
+
         <div className='flex flex-col flex-1'>
-          <Header />
+          <Header onClickLogout={logout} />
+
           <div className='p-6'>
             <Typography variant='h4' className='mb-8 ml-1'>
               {title}
             </Typography>
+
             {children}
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,21 @@
-import { AuthContext } from '@/shared/context';
 import { Button, Navbar, Typography } from '@material-tailwind/react';
 import Link from 'next/link';
-import { useContext } from 'react';
 
-export const Header = () => {
-  const { logout } = useContext(AuthContext);
-
+export const Header = ({ onClickLogout }: { onClickLogout: () => void }) => {
   return (
     <Navbar className='sticky top z-10 h-max max-w-full rounded-none py-3 px-6'>
       <div className='flex items-center justify-between'>
         <Typography className='mr-4 cursor-pointer py-1.5 font-bold text-blue-gray-900'>
           <Link href='/'>성적 관리 시스템</Link>
         </Typography>
-        <Button color='pink' variant='gradient' size='sm' className='inline-block' onClick={logout}>
+
+        <Button
+          color='pink'
+          variant='gradient'
+          size='sm'
+          className='inline-block'
+          onClick={onClickLogout}
+        >
           로그아웃
         </Button>
       </div>
